43ffeef3677919dcc33783e8ca3b2465b3239183
Halil Kitir <hak001@edu.zealand.dk>
2025-02-22 00:03:01 +0100

    Add project files.

---
+++ Trophy/Trophy.cs
@@ -0,0 +1,49 @@
+﻿namespace Trophy
+{
+    public class Trophy
+    {
+        private string competition;
+        private int year;
+
+        public int id { get; set; }
+        
+        public string Competition
+        {
+            get => competition;
+            set
+            {
+                if (value == null)
+                {
+                    throw new System.ArgumentNullException("Competition cannot be null");
+                }
+                if (value.Length < 3)
+                {
+                    throw new System.ArgumentException("Competition must be at least 3 characters long");
+                }
+                competition = value;
+            }
+        }
+
+        public int Year
+        {
+            get => year;
+            set
+            {
+                if (value < 1970)
+                {
+                    throw new System.ArgumentException("Year must be greater than 1970");
+                }
+                if (value > 2025)
+                {
+                    throw new System.ArgumentException("Year must be less than 2025");
+                }
+                year = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {id}, Competition: {Competition}, Year: {Year}";
+        }
+    }
+}
---
+++ TrophyTests/TrophyTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trophy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trophy.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void CompetitionNameLenght()
+        {
+            Trophy a = new Trophy();
+            Assert.ThrowsException<ArgumentException>(() => a.Competition = "Da");
+
+            Trophy b = new Trophy();
+            Assert.AreEqual("Champions League", b.Competition = "Champions League");
+
+            Trophy c = new Trophy();
+            Assert.ThrowsException<ArgumentNullException>(() => c.Competition = null);
+        }
+
+        [TestMethod()]
+        public void CompetitionYear()
+        {
+            Trophy a = new Trophy();
+            Assert.ThrowsException<ArgumentException>(() => a.Year = 1969);
+
+            Trophy b = new Trophy();
+            Assert.AreEqual(1970, b.Year = 1970);
+
+            Trophy c = new Trophy();
+            Assert.ThrowsException<ArgumentException>(() => c.Year = 2026);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Trophy b = new Trophy { id = 1, Competition = "Champions League", Year = 2022};
+            string expected = "Id: 1, Competition: Champions League, Year: 2022";
+            Assert.AreEqual(expected, b.ToString());
+        }
+    }
+}
